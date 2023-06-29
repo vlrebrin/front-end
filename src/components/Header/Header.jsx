639dd59136cab6451507336b9649f77e4dcf7630
Dedpc <vlrebrin@yandex.ru>
2023-06-29 12:36:00 +0700

    comm 7

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
  
   return (
     <div className='header'>
-      <Button onClick={onClose}>Закрыть</Button>
+      <Button className={'button'} onClick={onClose}>Закрыть</Button>
       <span className={'username'}>
         {user?.username}
       </span>      
